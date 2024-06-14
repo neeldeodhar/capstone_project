b29b50012f2d2f97e723b8feb2d6e30e940cfa54
Indraneel.Deodhar <ideodhar@gmail.com>
2024-06-13 20:07:43 -0700

    manipulation and visualization(almost complete)

--- capstone.project/analyzer.py
+++ capstone.project/analyzer.py
@@ -1,7 +1,16 @@
 import os
 import sys
 import pandas as pd
+import seaborn as sns
+import random
+import math
+import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.utils import shuffle
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import MinMaxScaler
 def read_dataset(csv_file_path: str) -> pd.DataFrame:
     dataset = pd.read_csv(filepath_or_buffer = csv_file_path)
     
@@ -10,19 +19,135 @@ def read_dataset(csv_file_path: str) -> pd.DataFrame:
 def describe(dataset: pd.DataFrame) ->str:
     print(dataset.describe())
     return dataset.describe()
+
+class DataManipulation():
+    def __init__(self, df:pd.DataFrame):
+   # id = Column(String)
+       # self.df = pd.DataFrame
+       self.df = df
+
+    def drop_column(self, column_name: str):
+        data_dfdf = self.df.copy()
+        
+        self.df = data_dfdf.drop(columns=column_name)
+        return self.df
+
+    def encode_features(self, column_names):
+        data_dfdf = self.df.copy()
+        label_encoder = LabelEncoder()
+       
+        for col in column_names:
+          data_dfdf[col] = label_encoder.fit_transform(data_dfdf[[col]])
+        self.df= data_dfdf
+        return self.df
+
+    def encode_label(self, column_name: str):
+        data_dfdf = self.df.copy()
+       
+        dummies = pd.get_dummies(data_dfdf[column_name])
+        dumcat = pd.concat([data_dfdf, dummies], axis =1)
+       
+        self.df= dumcat
+        return self.df
+    def shuffle(self): 
+        data_dfdf = shuffle(self.df)
+        self.df = data_dfdf
+       
+        return self.df
+    def min_max_Scaler(self, column_names):
+        data_dfdf = self.df.copy()
+        selected_features = self.df[column_names]
+        scaler = MinMaxScaler()
+        scaler.fit(selected_features)
+        scaler.transform(selected_features)
+        self.df= data_dfdf
+        return self.df
+    def z_Score(self, column_names):
+        data_dfdf = self.df.copy()
+        selected_features = self.df[column_names]
+        scaler = StandardScaler()
+        scaler.fit(selected_features)
+        scaler.transform(selected_features)
+        
+        mean = np.mean(selected_features)
+        std_dev = np.std(selected_features)
+        z_scores = (selected_features - mean) / std_dev
+        self.df= data_dfdf
+        return self.df
+    def sample(self , df:pd.DataFrame)-> pd.DataFrame:
+        data_dfdf = self.df.copy()
+        df = df.sample(frac=0.5, replace= True, random_state=1)
+        self.df= data_dfdf
+        return self.df
+  
+class visualization():
+    def __init__(self, df:pd.DataFrame):
+        self.df = df
+    def plot_pairPlot(self, column_names):
+       
+        selected_features = self.df[column_names]
+        plt.figure(figsize = (12,5))
+        sns.pairplot(selected_features)
+        plt.show()
+    def plot_correlationMatrix(self, column_names):
+        selected_features = self.df[column_names]
+       
+        plt.figure(figsize =(7,7))
+        sns.heatmap(selected_features.corr(), annot = True)
+       
+        plt.show()
+    def plot_boxPlot(self, column_names):
+        selected_features = self.df[column_names]
+        plt.figure(figsize = (12,5))
+        sns.boxplot(data = selected_features)
+        plt.show()
+    def plot_histograms_numeric(self,column_names):
+        selected_features = self.df[column_names]
+       # data = np.random.normal(170, 10, 250)
+
+        # Plot the histogram
+        plt.hist(selected_features)
+        plt.xlabel([column_names])
+        plt.ylabel("values")
+        plt.title("Numeric Histogram")
+        plt.show()
+
+    def plot_histograms_categorical(self,column_names):
+        selected_features = self.df[column_names]
+       # data = np.random.normal(170, 10, 250)
+
+        # Plot the histogram
+        plt.hist(selected_features)
+        plt.xlabel("categorical attributes")
+        plt.ylabel("values")
+        plt.title("categorical Histogram")
+        plt.show()
+
 if __name__ =="__main__":
+    
     absolute_path = "C:/Users/ideod/OneDrive/Documents/new folder zip data/diamonds.csv"
     dfdf = read_dataset(csv_file_path=absolute_path)
-    file_description = describe(dataset =dfdf)
+    original = DataManipulation(dfdf)
+  
+    dfdf = original.drop_column("Unnamed: 0")
+    dfdf = original.sample(dfdf)
+    dfdf = original.shuffle()
+    dfdf = original.encode_features(['carat','color','clarity'])
+    
+    dfdf = original.encode_label("cut")
+    dfdf = original.min_max_Scaler(['carat','color','clarity'])
+    dfdf = original.z_Score(['carat','color','clarity'])
+
+
+    visual = visualization(dfdf)
     print(describe)
-class DataManipulation:
-    id = Column(String)
-def drop_missing_data(self, data: pd.DataFrame) -> pd.DataFrame:
-    return data.dropna()
-def drop_columns(self, data: pd.DataFrame) -> pd.DataFrame:
-    return data.drop(columns = [''], axis = 1)
-labelencoder = LabelEncoder()
-def encoding(self, data: pd.DataFrame)-> pd.DataFrame:
-    return data.labelencoder()
+    print(dfdf)
+    visual.plot_pairPlot(['color','clarity', 'cut', 'depth','table'])
+  
+    visual.plot_correlationMatrix(['carat','depth','table','price','x','y','z'])
+    visual.plot_boxPlot(['carat','depth','table'])
+    visual.plot_histograms_numeric(['carat','color','clarity','depth','table'])
+    visual.plot_histograms_categorical(['Fair','Good','Ideal','Premium','Very Good'])
 
 
+    
